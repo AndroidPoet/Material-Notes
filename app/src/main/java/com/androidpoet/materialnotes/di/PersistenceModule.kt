8223e3cf50c1534da92b0aef6fd570518a036d8d
AndroidPoet <poetdroid2@gmail.com>
2022-03-21 19:16:50 +0530

    first commit

--- app/src/main/java/com/androidpoet/materialnotes/di/PersistenceModule.kt
+++ app/src/main/java/com/androidpoet/materialnotes/di/PersistenceModule.kt
@@ -1,11 +1,11 @@
 /*
- * Designed and developed by 2020 skydoves (Jaewoong Eum)
+ * Designed and developed by 2022 AndroidPoet (Ranbir Singh)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -30,20 +30,20 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object PersistenceModule {
 
-  @Provides
-  @Singleton
-  fun provideAppDatabase(
-    application: Application
-  ): AppDatabase {
-    return Room
-      .databaseBuilder(application, AppDatabase::class.java, "Notes.db")
-      .fallbackToDestructiveMigration()
-      .build()
-  }
+    @Provides
+    @Singleton
+    fun provideAppDatabase(
+        application: Application
+    ): AppDatabase {
+        return Room
+            .databaseBuilder(application, AppDatabase::class.java, "Notes.db")
+            .fallbackToDestructiveMigration()
+            .build()
+    }
 
-  @Provides
-  @Singleton
-  fun providePokemonDao(appDatabase: AppDatabase): NotesDao {
-    return appDatabase.notesDao()
-  }
+    @Provides
+    @Singleton
+    fun providePokemonDao(appDatabase: AppDatabase): NotesDao {
+        return appDatabase.notesDao()
+    }
 }
--- app/src/main/java/com/androidpoet/materialnotes/repository/MainRepository.kt
+++ app/src/main/java/com/androidpoet/materialnotes/repository/MainRepository.kt
@@ -1,11 +1,11 @@
 /*
- * Designed and developed by 2020 skydoves (Jaewoong Eum)
+ * Designed and developed by 2022 AndroidPoet (Ranbir Singh)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -23,23 +23,23 @@ import com.androidpoet.materialnotes.persistence.NotesDao
 import javax.inject.Inject
 
 class MainRepository @Inject constructor(
-  private val notesDao: NotesDao
+    private val notesDao: NotesDao
 ) {
 
-  val getAllNotes = Pager(
-    config = PagingConfig(pageSize = 10),
-    pagingSourceFactory = { notesDao.getNotesList() }
-  ).flow
+    val getAllNotes = Pager(
+        config = PagingConfig(pageSize = 10),
+        pagingSourceFactory = { notesDao.getNotesList() }
+    ).flow
 
-  suspend fun addNote(note: Note) {
-    notesDao.insertNote(note)
-  }
+    suspend fun addNote(note: Note) {
+        notesDao.insertNote(note)
+    }
 
-  suspend fun deleteNote(note: Note) {
-    notesDao.deleteNote(note)
-  }
+    suspend fun deleteNote(note: Note) {
+        notesDao.deleteNote(note)
+    }
 
-  suspend fun updateNote(note: Note) {
-    notesDao.updateNote(note)
-  }
+    suspend fun updateNote(note: Note) {
+        notesDao.updateNote(note)
+    }
 }
